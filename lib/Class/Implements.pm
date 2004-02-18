4546753b519ab6f7bc290341a85926ea0bac6ebe
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2004-02-18 23:59:57 +0000

    skeletons
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Class-Implements@2156 c56e198b-7db7-0310-b3ab-8e406f684e33

---
+++ lib/Class/Implements.pm
@@ -0,0 +1,49 @@
+package Class::Implements;
+
+use strict;
+#use warnings;
+
+
+
+=head1 NAME
+
+Class::Implements - 
+
+=head1 SYNOPSIS
+
+  use Class::Implements;
+
+
+
+=head1 DESCRIPTION
+
+
+
+=cut
+
+
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net>
+
+Copyright Richard Clamp 2004.  All Rights Reserved.
+
+This program is free software; you can redistribute it
+and/or modify it under the same terms as Perl itself.
+
+
+=head1 BUGS
+
+None known.
+
+Bugs should be reported to me via the CPAN RT system.
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Class::Implements>.
+
+
+=head1 SEE ALSO
+
+
+=cut
+
+1;
